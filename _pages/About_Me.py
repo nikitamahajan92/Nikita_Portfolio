12cdedffad61195bec8e5f04d282e38c0125b124
Nikita Mahajan <117332579+nikitamahajan92@users.noreply.github.com>
2025-08-04 14:52:27 +0530

    Update About_Me.py

--- _pages/About_Me.py
+++ _pages/About_Me.py
@@ -2,7 +2,7 @@
 right_column, left_column = st.columns([1, 2])
 with right_column:
     st.markdown("<br>", unsafe_allow_html=True)
-    st.image(r"/home/abcd/Downloads/Portfolio/Nikita_Portfolio/images/nikita.png", use_container_width=True)
+    st.image(r"images/nikita.png", use_container_width=True)
 
 with left_column:
     st.title("About Me")
